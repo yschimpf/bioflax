a06785d02558181d7dc7b12ea8f7995a53b391d2
yschimpf <yaschimpf@gmail.com>
2023-10-19 15:55:47 +0200

    testing module

--- layers/fa/RandomDenseLinear.py
+++ layers/fa/RandomDenseLinear.py
@@ -26,25 +26,35 @@ class RandomDenseLinear(nn.Module):
         def fwd(module, x):
             return nn.vjp(f, module, x)
         
-        B = self.param('B', nn.initializers.lecun_normal(), (self.features,jnp.shape(x)[-1]))
+        B = self.param("B", nn.initializers.lecun_normal(), (jnp.shape(x)[-1],self.features))
 
         def bwd(vjp_fn, delta):
             delta_params, _ = vjp_fn(delta)
-            delta_x = self.B.T @ delta
+            delta_x = B @ delta
             return (delta_params, delta_x)
         
         forward_module = nn.Dense(self.features)
         custom_f = nn.custom_vjp(fn = f, forward_fn = fwd, backward_fn = bwd)
         return custom_f(forward_module, x)
 
-
-model = RandomDenseLinear(features = 1)
+#testing stuff
+model = RandomDenseLinear(features = 3)
 params = model.init(jax.random.PRNGKey(0), jnp.ones((2,)))
-print(jnp.ones((3,)))
+print(jnp.ones((2,)))
 print(params) 
 
 x = jnp.array([1., 2.])
 y = model.apply(params, x)
 assert jnp.allclose(params["params"]["Dense_0"]["kernel"].T @ x + params["params"]["Dense_0"]["bias"], y)
 target = y-1
+
+def loss(params, x):
+    return 0.5 * jnp.sum((model.apply(params, x) - target)**2)
+
+delta_params, delta_x = jax.grad(loss, argnums=(0,1))(params, x)
+print("DELTA PARAMS")
+print(delta_params)
+print("DELTA X")
+print(delta_x)
+print(params["params"]["B"] @ jnp.array([1., 1., 1.]))
 

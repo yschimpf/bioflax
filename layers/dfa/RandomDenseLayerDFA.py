42fcd774595002df41f73b455bc8d86883c2f4cb
yschimpf <yaschimpf@gmail.com>
2023-10-20 16:32:29 +0200

    relu & sigmoid testing for dfa

--- layers/dfa/RandomDenseLayerDFA.py
+++ layers/dfa/RandomDenseLayerDFA.py
@@ -78,80 +78,157 @@ print(params["params"]["B"]@jnp.array([1,1,1]))
 
 
 """
-class Network(nn.Module):
-
-    @nn.compact
-    def __call__(self, x):
-        x = RandomDenseLinearDFAHidden(3,2,2, nn.sigmoid)(x)
-        x = RandomDenseLinearDFAOutput(2, nn.sigmoid)(x)
-        return x
-
-model = Network()
-params = model.init(jax.random.PRNGKey(0), jnp.ones((2,)))
-
-# Define the optimizer
-optimizer = optax.sgd(learning_rate=0.1)
-
-# Initialize the optimizer state
-optimizer_state = optimizer.init(params)
-
-
-x = jnp.array([1., 2.])
-y = model.apply(params, x)
-print("a_1")
-a_1 = params["params"]["RandomDenseLinearDFAHidden_0"]["Dense_0"]["kernel"].T @ x + params["params"]["RandomDenseLinearDFAHidden_0"]["Dense_0"]["bias"]
-print(a_1)
-
-print("h_1")
-h_1 = nn.sigmoid(a_1)
-print(h_1)
-
-print("a_2")
-a_2 = params["params"]["RandomDenseLinearDFAOutput_0"]["Dense_0"]["kernel"].T @ h_1 + params["params"]["RandomDenseLinearDFAOutput_0"]["Dense_0"]["bias"]
-print(a_2)
-
-print("h_2")
-h_2 = nn.sigmoid(a_2)
-print(h_2)
-
-assert jnp.allclose(h_2, y)
-
-target = y-1
-def loss(params, x):
-    return 0.5 * jnp.sum((model.apply(params, x)-target)**2)
-delta_params, delta_x = jax.grad(loss, argnums=(0,1))(params, x)
-print("DELTA PARAMS")
-print(delta_params)
-print("DELTA X")
-print(delta_x)
-
-#define a function that computer^s derivative of relu
-def relu_derivative(x):
-    return jnp.where(x > 0, 1, 0)
-
-#define a function that computes derivative of sigmoid
-def sigmoid_derivative(x):
-    return jnp.exp(-x) / (1 + jnp.exp(-x))**2
-
-print("dW_2")
-print(jnp.outer(h_1,jnp.ones((2,))*sigmoid_derivative(a_2)))
-
-print("dW_1")
-print(jnp.outer(x,(params["params"]["RandomDenseLinearDFAHidden_0"]["B"] @ jnp.ones((2,)) * sigmoid_derivative(a_1))))
 
 
 
+z = False
 
-# Compute the gradients of the loss function with respect to the parameters and the input
-delta_params, delta_x = jax.grad(loss, argnums=(0,1))(params, x)
+if(z): #do everything for sigmoid
+    class Network(nn.Module):
 
-# Apply the gradient update to the parameters using the optimizer
-updates, new_optimizer_state = optimizer.update(delta_params, optimizer_state)
-new_params = optax.apply_updates(params, updates)
+        @nn.compact
+        def __call__(self, x):
+            x = RandomDenseLinearDFAHidden(3,2,2, nn.sigmoid)(x)
+            x = RandomDenseLinearDFAOutput(2, nn.sigmoid)(x)
+            return x
 
-# Update the parameters of the model
-print(params)
-print(new_params)
+    model = Network()
+    params = model.init(jax.random.PRNGKey(0), jnp.ones((2,)))
 
-# Update the optimizer state
-optimizer_state = new_optimizer_state
+    # Define the optimizer
+    optimizer = optax.sgd(learning_rate=0.1)
+
+    # Initialize the optimizer state
+    optimizer_state = optimizer.init(params)
+    x = jnp.array([1., 2.])
+    y = model.apply(params, x)
+    print("a_1")
+    a_1 = params["params"]["RandomDenseLinearDFAHidden_0"]["Dense_0"]["kernel"].T @ x + params["params"]["RandomDenseLinearDFAHidden_0"]["Dense_0"]["bias"]
+    print(a_1)
+
+    print("h_1")
+    h_1 = nn.sigmoid(a_1)
+    print(h_1)
+
+    print("a_2")
+    a_2 = params["params"]["RandomDenseLinearDFAOutput_0"]["Dense_0"]["kernel"].T @ h_1 + params["params"]["RandomDenseLinearDFAOutput_0"]["Dense_0"]["bias"]
+    print(a_2)
+
+    print("h_2")
+    h_2 = nn.sigmoid(a_2)
+    print(h_2)
+
+    assert jnp.allclose(h_2, y)
+
+    target = y-1
+    def loss(params, x):
+        return 0.5 * jnp.sum((model.apply(params, x)-target)**2)
+    delta_params, delta_x = jax.grad(loss, argnums=(0,1))(params, x)
+    print("DELTA PARAMS")
+    print(delta_params)
+    print("DELTA X")
+    print(delta_x)
+
+    #define a function that computer^s derivative of relu
+    def relu_derivative(x):
+        return jnp.where(x > 0, 1, 0)
+
+    #define a function that computes derivative of sigmoid
+    def sigmoid_derivative(x):
+        return jnp.exp(-x) / (1 + jnp.exp(-x))**2
+
+    print("dW_2")
+    print(jnp.outer(h_1,jnp.ones((2,))*sigmoid_derivative(a_2)))
+
+    print("dW_1")
+    print(jnp.outer(x,(params["params"]["RandomDenseLinearDFAHidden_0"]["B"] @ jnp.ones((2,)) * sigmoid_derivative(a_1))))
+
+
+
+
+    # Compute the gradients of the loss function with respect to the parameters and the input
+    delta_params, delta_x = jax.grad(loss, argnums=(0,1))(params, x)
+
+    # Apply the gradient update to the parameters using the optimizer
+    updates, new_optimizer_state = optimizer.update(delta_params, optimizer_state)
+    new_params = optax.apply_updates(params, updates)
+
+    # Update the parameters of the model
+    print(params)
+    print(new_params)
+
+    # Update the optimizer state
+    optimizer_state = new_optimizer_state
+
+else: #do everything for relu
+    class Network(nn.Module):
+
+        @nn.compact
+        def __call__(self, x):
+            x = RandomDenseLinearDFAHidden(3,2,2)(x)
+            x = RandomDenseLinearDFAOutput(2)(x)
+            return x
+
+    model = Network()
+    params = model.init(jax.random.PRNGKey(0), jnp.ones((2,)))
+
+    # Define the optimizer
+    optimizer = optax.sgd(learning_rate=0.1)
+
+    # Initialize the optimizer state
+    optimizer_state = optimizer.init(params)
+    x = jnp.array([1., 2.])
+    y = model.apply(params, x)
+    print("a_1")
+    a_1 = params["params"]["RandomDenseLinearDFAHidden_0"]["Dense_0"]["kernel"].T @ x + params["params"]["RandomDenseLinearDFAHidden_0"]["Dense_0"]["bias"]
+    print(a_1)
+
+    print("h_1")
+    h_1 = nn.relu(a_1)
+    print(h_1)
+
+    print("a_2")
+    a_2 = params["params"]["RandomDenseLinearDFAOutput_0"]["Dense_0"]["kernel"].T @ h_1 + params["params"]["RandomDenseLinearDFAOutput_0"]["Dense_0"]["bias"]
+    print(a_2)
+
+    print("h_2")
+    h_2 = nn.relu(a_2)
+    print(h_2)
+
+    assert jnp.allclose(h_2, y)
+
+    target = y-1
+    def loss(params, x):
+        return 0.5 * jnp.sum((model.apply(params, x)-target)**2)
+    delta_params, delta_x = jax.grad(loss, argnums=(0,1))(params, x)
+    print("DELTA PARAMS")
+    print(delta_params)
+    print("DELTA X")
+    print(delta_x)
+
+    #define a function that computer^s derivative of relu
+    def relu_derivative(x):
+        return jnp.where(x > 0, 1, 0)
+
+    print("dW_2")
+    print(jnp.outer(h_1,jnp.ones((2,))*relu_derivative(a_2)))
+
+    print("dW_1")
+    print(jnp.outer(x,(params["params"]["RandomDenseLinearDFAHidden_0"]["B"] @ jnp.ones((2,)) * relu_derivative(a_1))))
+
+
+
+
+    # Compute the gradients of the loss function with respect to the parameters and the input
+    delta_params, delta_x = jax.grad(loss, argnums=(0,1))(params, x)
+
+    # Apply the gradient update to the parameters using the optimizer
+    updates, new_optimizer_state = optimizer.update(delta_params, optimizer_state)
+    new_params = optax.apply_updates(params, updates)
+
+    # Update the parameters of the model
+    print(params)
+    print(new_params)
+
+    # Update the optimizer state
+    optimizer_state = new_optimizer_state

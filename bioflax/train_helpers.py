2fe491a7f15a56a08f2744d9aafa7b08bf58ebc8
NicolasZucchet <nzucchet@ethz.ch>
2023-11-14 13:40:28 +0100

    Faster compilation time for eval_step

--- bioflax/train_helpers.py
+++ bioflax/train_helpers.py
@@ -12,11 +12,7 @@ from jax.nn import one_hot
 from tqdm import tqdm
 from flax.training import train_state
 from functools import partial
-from .metric_computation import (
-    compute_metrics,
-    summarize_metrics_epoch,
-    reorganize_dict
-)
+from .metric_computation import compute_metrics, summarize_metrics_epoch, reorganize_dict
 
 
 def create_train_state(model, rng, lr, momentum, weight_decay, in_dim, batch_size, seq_len):
@@ -41,12 +37,9 @@ def create_train_state(model, rng, lr, momentum, weight_decay, in_dim, batch_siz
         sequence length used when running model
     """
     dummy_input = jnp.ones((batch_size, in_dim, seq_len))
-    params = model.init(rng, dummy_input)['params']
+    params = model.init(rng, dummy_input)["params"]
     sgd_optimizer = optax.sgd(learning_rate=lr, momentum=momentum)
-    tx = optax.chain(
-        optax.add_decayed_weights(weight_decay),
-        sgd_optimizer
-    )
+    tx = optax.chain(optax.add_decayed_weights(weight_decay), sgd_optimizer)
     return train_state.TrainState.create(apply_fn=model.apply, params=params, tx=tx)
 
 
@@ -85,13 +78,16 @@ def train_epoch(state, model, trainloader, loss_function, n, mode, compute_align
         inputs, labels = prep_batch(batch)
 
         if i < n and compute_alignments:
+
             def loss_comp(params):
-                logits = model.apply({'params': params}, inputs)
+                logits = model.apply({"params": params}, inputs)
                 loss = get_loss(loss_function, logits, labels)
                 return loss
-            if (mode != 'bp'):
+
+            if mode != "bp":
                 _, grads_ = jax.value_and_grad(loss_comp)(
-                    reorganize_dict({'params': state.params})["params"])
+                    reorganize_dict({"params": state.params})["params"]
+                )
             else:
                 _, grads_ = jax.value_and_grad(loss_comp)(state.params)
 
@@ -99,8 +95,13 @@ def train_epoch(state, model, trainloader, loss_function, n, mode, compute_align
         batch_losses.append(loss)
 
         if i < n and compute_alignments:
-            bias_al_per_layer, wandb_grad_al_per_layer, wandb_grad_al_total, weight_al_per_layer, rel_norm_grads = compute_metrics(
-                state, grads_,  grads, mode)
+            (
+                bias_al_per_layer,
+                wandb_grad_al_per_layer,
+                wandb_grad_al_total,
+                weight_al_per_layer,
+                rel_norm_grads,
+            ) = compute_metrics(state, grads_, grads, mode)
 
             bias_als_per_layer.append(bias_al_per_layer)
             wandb_grad_als_per_layer.append(wandb_grad_al_per_layer)
@@ -109,9 +110,29 @@ def train_epoch(state, model, trainloader, loss_function, n, mode, compute_align
             rel_norms_grads.append(rel_norm_grads)
 
     if compute_alignments:
-        avg_bias_al_per_layer, avg_wandb_grad_al_per_layer, avg_wandb_grad_al_total, avg_weight_al_per_layer, avg_rel_norm_grads = summarize_metrics_epoch(
-            bias_als_per_layer, wandb_grad_als_per_layer, wandb_grad_als_total, weight_als_per_layer, rel_norms_grads, mode)
-        return state, jnp.mean(jnp.array(batch_losses)), avg_bias_al_per_layer, avg_wandb_grad_al_per_layer, avg_wandb_grad_al_total, avg_weight_al_per_layer, avg_rel_norm_grads
+        (
+            avg_bias_al_per_layer,
+            avg_wandb_grad_al_per_layer,
+            avg_wandb_grad_al_total,
+            avg_weight_al_per_layer,
+            avg_rel_norm_grads,
+        ) = summarize_metrics_epoch(
+            bias_als_per_layer,
+            wandb_grad_als_per_layer,
+            wandb_grad_als_total,
+            weight_als_per_layer,
+            rel_norms_grads,
+            mode,
+        )
+        return (
+            state,
+            jnp.mean(jnp.array(batch_losses)),
+            avg_bias_al_per_layer,
+            avg_wandb_grad_al_per_layer,
+            avg_wandb_grad_al_total,
+            avg_weight_al_per_layer,
+            avg_rel_norm_grads,
+        )
     else:
         return state, jnp.mean(jnp.array(batch_losses)), 0, 0, 0, 0, 0
 
@@ -132,10 +153,12 @@ def train_step(state, inputs, labels, loss_function):
     loss_function: str
         identifier to select loss function
     """
+
     def loss_fn(params):
-        logits = state.apply_fn({'params': params}, inputs)
+        logits = state.apply_fn({"params": params}, inputs)
         loss = get_loss(loss_function, logits, labels)
         return loss
+
     loss, grads = jax.value_and_grad(loss_fn)(state.params)
     state = state.apply_gradients(grads=grads)
     return state, loss, grads
@@ -154,9 +177,11 @@ def get_loss(loss_function, logits, labels):
     labels : int32
         labels for the batch
     """
-    if (loss_function == "CE"):
-        return optax.softmax_cross_entropy_with_integer_labels(logits=jnp.squeeze(logits), labels=labels).mean()
-    elif (loss_function == "MSE"):
+    if loss_function == "CE":
+        return optax.softmax_cross_entropy_with_integer_labels(
+            logits=jnp.squeeze(logits), labels=labels
+        ).mean()
+    elif loss_function == "MSE":
         return optax.l2_loss(jnp.squeeze(logits), jnp.squeeze(labels)).mean()
 
 
@@ -212,7 +237,7 @@ def eval_step(inputs, labels, state, loss_function):
     """
     logits = state.apply_fn({"params": state.params}, inputs)
     losses = get_loss(loss_function, logits, labels)
-    accs = None
+    accs = 0
     if loss_function == "CE":
         accs = compute_accuracy((jnp.squeeze(logits)), labels)
     return jnp.mean(losses), accs, logits
@@ -235,20 +260,19 @@ def validate(state, testloader, seq_len, in_dim, loss_function):
     loss_function: str
         identifier to select loss function
     """
-    losses, accuracies = jnp.array([]), jnp.array([])
+    losses, accuracies = [], []
 
     for batch in tqdm(testloader):
         inputs, labels = prep_batch(batch)
         loss, acc, _ = eval_step(inputs, labels, state, loss_function)
-        losses = jnp.append(losses, loss)
+        losses.append(loss)
         if loss_function == "CE":
-            accuracies = jnp.append(accuracies, acc)
-        loss_mean = jnp.mean(losses)
+            accuracies.append(jnp.mean(acc))
 
-    acc_mean = 10000000.
+    acc_mean = 10000000.0
     if loss_function == "CE":
-        acc_mean = jnp.mean(accuracies)
-    return loss_mean, acc_mean
+        acc_mean = jnp.mean(jnp.array([accuracies]))
+    return jnp.mean(jnp.array([losses])), acc_mean
 
 
 def pred_step(state, batch, task):
@@ -261,14 +285,14 @@ def pred_step(state, batch, task):
         current train state of the model
     batch : tuple
         batch of data
-    task : str 
+    task : str
         identifier for task
     """
     inputs, _ = prep_batch(batch)
-    logits = state.apply_fn({'params': state.params}, inputs)
-    if (task == "classification"):
+    logits = state.apply_fn({"params": state.params}, inputs)
+    if task == "classification":
         return jnp.squeeze(logits).argmax(axis=1)
-    elif (task == "regression"):
+    elif task == "regression":
         return logits
     else:
         print("Task not supported")
@@ -292,9 +316,9 @@ def plot_sample(testloader, state, seq_len, in_dim, task, output_features):
     task : str
         identifier for task
     """
-    if (task == "classification"):
+    if task == "classification":
         return plot_mnist_sample(testloader, state, task)
-    elif (task == "regression"):
+    elif task == "regression":
         return plot_regression_sample(testloader, state, seq_len, in_dim, task, output_features)
     else:
         print("Task not supported")
@@ -322,9 +346,9 @@ def plot_mnist_sample(testloader, state, task):
     inputs = torch.reshape(inputs, (inputs.shape[0], 28, 28, 1))
     print(inputs.shape)
     for i, ax in enumerate(axs.flatten()):
-        ax.imshow(inputs[i, ..., 0], cmap='gray')
+        ax.imshow(inputs[i, ..., 0], cmap="gray")
         ax.set_title(f"label={pred[i]}")
-        ax.axis('off')
+        ax.axis("off")
     plt.show()
 
 

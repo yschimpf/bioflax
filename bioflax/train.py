7118aef1a5643469dcd7bf52c482ebefbff25764
NicolasZucchet <nzucchet@ethz.ch>
2023-11-14 09:18:49 +0100

    Small cosmetic changes

--- bioflax/train.py
+++ bioflax/train.py
@@ -1,23 +1,15 @@
 import wandb
 from jax import random
 from typing import Any
-from .model import (
-    BatchBioNeuralNetwork
-)
-from .train_helpers import (
-    create_train_state,
-    train_epoch,
-    validate,
-    plot_sample
-)
-from .dataloading import (
-    create_dataset
-)
+from .model import BatchBioNeuralNetwork
+from .train_helpers import create_train_state, train_epoch, validate, plot_sample
+from .dataloading import create_dataset
 
 
 def train(args):
     """
-    Main function for training and eveluating a biomodel. Training and evaluation set up by arguments passed in args.
+    Main function for training and eveluating a biomodel. Training and evaluation set up by
+    arguments passed in args.
     """
 
     best_test_loss = 100000000
@@ -45,12 +37,11 @@ def train(args):
     project = args.wandb_project
     use_wandb = args.use_wandb
     n = args.n
-    entity = args.wandb_entity
 
-    key = random.PRNGKey(seed)
-    if (dataset == "mnist"):
+    if dataset == "mnist":
         task = "classification"
         loss_fn = "CE"
+        output_features = 10
     else:
         task = "regression"
         loss_fn = "MSE"
@@ -58,21 +49,18 @@ def train(args):
     if dataset == "sinreg":
         output_features = 1
 
-    if use_wandb:  # args.use_wandb:
+    if use_wandb:
         # Make wandb config dictionary
         wandb.init(
             project=project,
             job_type="model_training",
             config=vars(args),
-            entity=entity,
         )
-    else:
-        wandb.init(mode="offline")
 
     # Set seed for randomness
     print("[*] Setting Randomness...")
-
-    init_rng, key = random.split(key, num=2)
+    key = random.PRNGKey(seed)
+    key_data, key_model, key_model_bp = random.split(key, num=3)
 
     # Create dataset
     (
@@ -82,20 +70,30 @@ def train(args):
         output_features,
         seq_len,
         in_dim,
-    ) = create_dataset(seed=42, batch_size=batch_size, dataset=dataset, val_split=val_split, input_dim=in_dim, output_dim=output_features, L=seq_len, train_set_size=train_set_size, test_set_size=test_set_size, teacher_act=teacher_act)  # (seed=args.jax_seed, batch_size=args.batch_size, dataset = args.dataset)
-    print(f"[*] Starting training on mnist =>> '{dataset}' Initializing...")
+    ) = create_dataset(
+        seed=key_data[0].item(),
+        batch_size=batch_size,
+        dataset=dataset,
+        val_split=val_split,
+        input_dim=in_dim,
+        output_dim=output_features,
+        L=seq_len,
+        train_set_size=train_set_size,
+        test_set_size=test_set_size,
+        teacher_act=teacher_act,
+    )
+    print(f"[*] Starting training on '{dataset}'...")
 
-    # model to run experiments with
+    # Model to run experiments with
     model = BatchBioNeuralNetwork(
         hidden_layers=hidden_layers,
         activations=activations,
         features=output_features,
         mode=mode,
     )
-
     state = create_train_state(
         model=model,
-        rng=init_rng,
+        rng=key_model,
         lr=lr,
         momentum=momentum,
         weight_decay=weight_decay,
@@ -104,19 +102,16 @@ def train(args):
         seq_len=seq_len,
     )
 
-    init_rng_bp, key = random.split(key, num=2)
-
-    # bp model to compute alignments
+    # Backpropagation model to compute alignments
     bp_model = BatchBioNeuralNetwork(
         hidden_layers=hidden_layers,
         activations=activations,
         features=output_features,
         mode="bp",
     )
-
-    bp_state = create_train_state(
+    _ = create_train_state(
         model=bp_model,
-        rng=init_rng_bp,
+        rng=key_model_bp,
         lr=lr,
         momentum=momentum,
         weight_decay=weight_decay,
@@ -125,60 +120,50 @@ def train(args):
         seq_len=seq_len,
     )
 
-    # Training Loop over epochs
-    best_loss, best_acc, best_epoch = 100000000, - \
-        100000000.0, 0  # This best loss is val_loss
+    # Training loop over epochs
+    best_loss, best_acc, best_epoch = 100000000, -100000000.0, 0  # This best loss is val_loss
     for epoch in range(epochs):  # (args.epochs):
-        print(f"[*] Starting Training Epoch {epoch + 1}...")
-
-        state, train_loss, avg_bias_al_per_layer, avg_wandb_grad_al_per_layer, avg_wandb_grad_al_total, avg_weight_al_per_layer, avg_rel_norm_grads = train_epoch(
-            state, bp_model, trainloader, loss_fn, n, mode, compute_alignments
-        )
+        print(f"[*] Starting training epoch {epoch + 1}...")
+        (
+            state,
+            train_loss,
+            avg_bias_al_per_layer,
+            avg_wandb_grad_al_per_layer,
+            avg_wandb_grad_al_total,
+            avg_weight_al_per_layer,
+            avg_rel_norm_grads,
+        ) = train_epoch(state, bp_model, trainloader, loss_fn, n, mode, compute_alignments)
 
         if valloader is not None:
             print(f"[*] Running Epoch {epoch + 1} Validation...")
-            val_loss, val_acc = validate(
-                state, valloader, seq_len, in_dim, loss_fn)
+            val_loss, val_acc = validate(state, valloader, seq_len, in_dim, loss_fn)
 
             print(f"[*] Running Epoch {epoch + 1} Test...")
-            test_loss, test_acc = validate(
-                state, testloader, seq_len, in_dim, loss_fn)
+            test_loss, test_acc = validate(state, testloader, seq_len, in_dim, loss_fn)
 
             print(f"\n=>> Epoch {epoch + 1} Metrics ===")
             print(
                 f"\tTrain Loss: {train_loss:.5f} "
                 f"-- Val Loss: {val_loss:.5f} "
-                f"-- Test Loss: {test_loss:.5f}")
-            if task == 'classification':
-                print(
-                    f"\tVal Accuracy: {val_acc:.4f} "
-                    f"-- Test Accuracy: {test_acc:.4f} "
-                )
-            if compute_alignments:
-                print(
-                    f"\tRelative Norm Gradients: {avg_rel_norm_grads:.4f} "
-                    f"-- Gradient Alignment: {avg_wandb_grad_al_total:.4f}"
-                )
-
-        else:
-            # else use test set as validation set
-            print(f"[*] Running Epoch {epoch + 1} Test...")
-            val_loss, val_acc = validate(
-                state, testloader, seq_len, in_dim, loss_fn
+                f"-- Test Loss: {test_loss:.5f}"
             )
+            if task == "classification":
+                print(f"\tVal Accuracy: {val_acc:.4f} " f"-- Test Accuracy: {test_acc:.4f} ")
+        else:
+            # Use test set as validation set
+            print(f"[*] Running Epoch {epoch + 1} Test...")
+            val_loss, val_acc = validate(state, testloader, seq_len, in_dim, loss_fn)
 
             print(f"\n=>> Epoch {epoch + 1} Metrics ===")
+            print(f"\tTrain loss: {train_loss:.5f}" f"-- Test loss: {val_loss:.5f}")
+            if task == "classification":
+                print(f"-- Test accuracy: {val_acc:.4f}\n")
+
+        if compute_alignments:
             print(
-                f"\tTrain Loss: {train_loss:.5f}"
-                f"-- Test Loss: {val_loss:.5f}"
+                f"\tRelative norm gradients: {avg_rel_norm_grads:.4f} "
+                f"-- Gradient alignment: {avg_wandb_grad_al_total:.4f}"
             )
-            if task == 'classification':
-                print(f"-- Test Accuracy: {val_acc:.4f}\n")
-            if compute_alignments:
-                print(
-                    f"\tRelative Norm Gradients: {avg_rel_norm_grads:.4f}"
-                    f"-- Gradient Alignment: {avg_wandb_grad_al_total:.4f}"
-                )
 
         if val_acc > best_acc:
             # Increment counters etc.
@@ -187,35 +172,36 @@ def train(args):
                 best_test_loss, best_test_acc = test_loss, test_acc
             else:
                 best_test_loss, best_test_acc = best_loss, best_acc
+
         # Print best accuracy & loss so far...
-        if task == 'regression':
+        if task == "regression":
             print(
-                f"\tBest Val Loss: {best_loss:.5f} at Epoch {best_epoch + 1}\n"
-                f"\tBest Test Loss: {best_test_loss:.5f} at Epoch {best_epoch + 1}\n"
+                f"\tBest val loss: {best_loss:.5f} at epoch {best_epoch + 1}\n"
+                f"\tBest test loss: {best_test_loss:.5f} at epoch {best_epoch + 1}\n"
             )
-        elif task == 'classification':
+        elif task == "classification":
             print(
-                f"\tBest Val Loss: {best_loss:.5f} -- Best Val Accuracy:"
-                f" {best_acc:.4f} at Epoch {best_epoch + 1}\n"
-                f"\tBest Test Loss: {best_test_loss:.5f} -- Best Test Accuracy:"
-                f" {best_test_acc:.4f} at Epoch {best_epoch + 1}\n"
+                f"\tBest val loss: {best_loss:.5f} -- Best val accuracy:"
+                f" {best_acc:.4f} at epoch {best_epoch + 1}\n"
+                f"\tBest test loss: {best_test_loss:.5f} -- Best test accuracy:"
+                f" {best_test_acc:.4f} at epoch {best_epoch + 1}\n"
             )
 
         metrics = {
-            "Training Loss": train_loss,
-            "Val Loss": val_loss,
+            "Training loss": train_loss,
+            "Val loss": val_loss,
         }
 
-        if task == 'classification':
-            metrics["Val Accuracy"]: val_acc
+        if task == "classification":
+            metrics["Val accuracy"]: val_acc
         if valloader is not None:
-            metrics["Test Loss"] = test_loss
-            if task == 'classification':
-                metrics["Test Accuracy"] = test_acc
+            metrics["Test loss"] = test_loss
+            if task == "classification":
+                metrics["Test accuracy"] = test_acc
 
         if compute_alignments:
-            metrics["Relative Norms Gradients"] = avg_rel_norm_grads
-            metrics["Gradient Alignment"] = avg_wandb_grad_al_total
+            metrics["Relative norms gradients"] = avg_rel_norm_grads
+            metrics["Gradient alignment"] = avg_wandb_grad_al_total
             for i, al in enumerate(avg_bias_al_per_layer):
                 metrics[f"Alignment bias gradient layer {i}"] = al
             for i, al in enumerate(avg_wandb_grad_al_per_layer):
@@ -224,14 +210,14 @@ def train(args):
                 for i, al in enumerate(avg_weight_al_per_layer):
                     metrics[f"Alignment layer {i}"] = al
 
-        wandb.log(metrics)
+        if use_wandb:
+            wandb.log(metrics)
+            wandb.run.summary["Best val loss"] = best_loss
+            wandb.run.summary["Best epoch"] = best_epoch
+            wandb.run.summary["Best test loss"] = best_test_loss
+            if task == "classification":
+                wandb.run.summary["Best test accuracy"] = best_test_acc
+                wandb.run.summary["Best val accuracy"] = best_acc
 
-        wandb.run.summary["Best Val Loss"] = best_loss
-        wandb.run.summary["Best Epoch"] = best_epoch
-        wandb.run.summary["Best Test Loss"] = best_test_loss
-        if task == 'classification':
-            wandb.run.summary["Best Test Accuracy"] = best_test_acc
-            wandb.run.summary["Best Val Accuracy"] = best_acc
-
-    if (plot):
+    if plot:
         plot_sample(testloader, state, seq_len, in_dim, task, output_features)
--- run_train.py
+++ run_train.py
@@ -17,60 +17,128 @@ def str2bool(v):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser = argparse.ArgumentParser(
-        description="Parse command-line arguments for model training.")
+    parser = argparse.ArgumentParser(description="Parse command-line arguments for model training.")
 
-    parser.add_argument("--batch_size", type=int, default=32,
-                        help="Batch size during training. Type: int, Default: 32")
-    parser.add_argument("--val_split", type=float, default=0.1,
-                        help="Percentage of training samples for validation. Type: float, Default: 0.1")
-    parser.add_argument("--epochs", type=int, default=5,
-                        help="Number of training epochs. Type: int, Default: 5")
-    parser.add_argument("--mode", type=str, default="fa",
-                        choices=['bp', 'fa', 'kp', 'dfa'],
-                        help="Training mode. Choices: ['bp', 'fa', 'kp', 'dfa']. Type: str, Default: 'fa'")
-    parser.add_argument("--activations", nargs='+', type=str, default=["relu", "relu"],
-                        help="Activation functions for each layer. Type: str (list), Default: ['relu', 'relu']")
-    parser.add_argument("--hidden_layers", nargs='+', type=int, default=[500, 500],
-                        help="Neurons in each hidden layer. Type: int (list), Default: [500, 500]")
-    parser.add_argument("--jax_seed", type=int, default=0,
-                        help="Seed for JAX RNG. Type: int, Default: 0")
-    parser.add_argument("--dataset", type=str, default="mnist",
-                        choices=['mnist', 'sinreg', 'teacher'],
-                        help="Dataset for training. Choices: ['mnist', 'sinprop', 'teacher'], Type: str, Default: 'mnist'")
-    parser.add_argument("--in_dim", type=int, default=1,
-                        help="Input vector dimension. Type: int, Default: 1")
-    parser.add_argument("--seq_len", type=int, default=1,
-                        help="Length of input sequence. Type: int, Default: 1")
-    parser.add_argument("--output_features", type=int, default=10,
-                        help="Number of output features. Type: int, Default: 10")
-    parser.add_argument("--train_set_size", type=int, default=50,
-                        help="Size of the training set. Type: int, Default: 50")
-    parser.add_argument("--test_set_size", type=int, default=10,
-                        help="Size of the test set. Type: int, Default: 10")
-    parser.add_argument("--teacher_act", type=str, default="sigmoid",
-                        help="Activation function of teacher network. Type: str, Default: 'sigmoid'")
-    parser.add_argument("--lr", type=float, default=0.02,
-                        help="Learning rate. Type: float, Default: 0.02")
-    parser.add_argument("--momentum", type=float, default=0,
-                        help="Momentum value. Type: float, Default: 0")
-    parser.add_argument("--weight_decay", type=float, default=0,
-                        help="Weight decay for learning. Type: float, Default: 0.")
-    parser.add_argument("--plot", type=str2bool, default=True,
-                        help="Enable/disable plotting. Type: boolean, Default: True")
-    parser.add_argument("--compute_alignments", type=str2bool, default=True,
-                        help="Enable/disable alignment computations. Type: boolean, Default: True")
-    parser.add_argument("--wandb_project", type=str, default="test_project",
-                        help="Weights & Biases project name. Type: str, Default: 'test_project'")
-    parser.add_argument("--use_wandb", type=str2bool, default=True,
-                        help="Enable/disable Weights & Biases for logging. Type: boolean, Default: True")
-    parser.add_argument("--n", type=int, default=5,
-                        help="Batches for alignment averaging. Type: int, Default: 5")
-    parser.add_argument("--wandb_entity", type=str, default="bioflax",
-                        help="Weights & Biases entity name. Type: str, Default: 'bioflax'")
+    parser.add_argument(
+        "--jax_seed", type=int, default=0, help="Seed for JAX RNG. Type: int, Default: 0"
+    )
 
+    # Data
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="mnist",
+        choices=["mnist", "sinreg", "teacher"],
+        help="Dataset for training. Choices: ['mnist', 'sinprop', 'teacher'], Type: str, Default: 'mnist'",
+    )
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=32,
+        help="Batch size during training. Type: int, Default: 32",
+    )
+    parser.add_argument(
+        "--train_set_size",
+        type=int,
+        default=50,
+        help="Size of the training set. Type: int, Default: 50",
+    )
+    parser.add_argument(
+        "--test_set_size", type=int, default=10, help="Size of the test set. Type: int, Default: 10"
+    )
+    parser.add_argument(
+        "--val_split",
+        type=float,
+        default=0.1,
+        help="Percentage of training samples for validation. Type: float, Default: 0.1",
+    )
+    parser.add_argument(
+        "--in_dim", type=int, default=1, help="Input vector dimension. Type: int, Default: 1"
+    )
+    parser.add_argument(
+        "--seq_len", type=int, default=1, help="Length of input sequence. Type: int, Default: 1"
+    )
+    parser.add_argument(
+        "--output_features",
+        type=int,
+        default=10,
+        help="Number of output features. Type: int, Default: 10",
+    )
+    parser.add_argument(
+        "--teacher_act",
+        type=str,
+        default="sigmoid",
+        help="Activation function of teacher network. Type: str, Default: 'sigmoid'",
+    )
+
+    # Network
+    parser.add_argument(
+        "--mode",
+        type=str,
+        default="fa",
+        choices=["bp", "fa", "kp", "dfa"],
+        help="Training mode. Choices: ['bp', 'fa', 'kp', 'dfa']. Type: str, Default: 'fa'",
+    )
+    parser.add_argument(
+        "--activations",
+        nargs="+",
+        type=str,
+        default=["relu", "relu"],
+        help="Activation functions for each layer. Type: str (list), Default: ['relu', 'relu']",
+    )
+    parser.add_argument(
+        "--hidden_layers",
+        nargs="+",
+        type=int,
+        default=[500, 500],
+        help="Neurons in each hidden layer. Type: int (list), Default: [500, 500]",
+    )
+
+    # Optimizer
+    parser.add_argument(
+        "--epochs", type=int, default=5, help="Number of training epochs. Type: int, Default: 5"
+    )
+    parser.add_argument(
+        "--lr", type=float, default=0.02, help="Learning rate. Type: float, Default: 0.02"
+    )
+    parser.add_argument(
+        "--momentum", type=float, default=0, help="Momentum value. Type: float, Default: 0"
+    )
+    parser.add_argument(
+        "--weight_decay",
+        type=float,
+        default=0,
+        help="Weight decay for learning. Type: float, Default: 0.",
+    )
+
+    # Logging
+    parser.add_argument(
+        "--wandb_project",
+        type=str,
+        default="test_project",
+        help="Weights & Biases project name. Type: str, Default: 'bioflax'",
+    )
+    parser.add_argument(
+        "--use_wandb",
+        type=str2bool,
+        default=False,
+        help="Enable/disable Weights & Biases for logging. Type: boolean, Default: True",
+    )
+    parser.add_argument(
+        "--plot",
+        type=str2bool,
+        default=False,
+        help="Enable/disable plotting. Type: boolean, Default: False",
+    )
+    parser.add_argument(
+        "--compute_alignments",
+        type=str2bool,
+        default=True,
+        help="Enable/disable alignment computations. Type: boolean, Default: True",
+    )
+    parser.add_argument(
+        "--n", type=int, default=5, help="Batches for alignment averaging. Type: int, Default: 5"
+    )
     args = parser.parse_args()
 
-    print("Start")
     train(args)
-    print("End")
